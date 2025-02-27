d20b36845aacd898ddf721d29239fce33667a324
HuynhQuocTien <quoctien01062003@gmail.com>
2025-02-27 13:36:44 +0700

    Refactor project configuration and add user registration endpoint

--- accounts/models.py
+++ accounts/models.py
@@ -15,6 +15,7 @@ class UserManager(BaseUserManager):
         extra_field.setdefault('is_staff', True)
         extra_field.setdefault('is_superuser', True)
         return self.create_user(email, password, **extra_field)
+
 class User(AbstractUser,PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=255, blank=True)
@@ -22,8 +23,11 @@ class User(AbstractUser,PermissionsMixin):
     is_active=models.BooleanField(default=True)
     is_staff=models.BooleanField(default=False)
     date_joined=models.DateTimeField(auto_now_add=True)
+
     objects=UserManager()
+
     USERNAME_FIELD='email'
+    REQUIRED_FIELDS = ['username']
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
